031243d90e895909ee9347e90768b3ed3b8f68b7
REM-IX <60319926+REM-IX@users.noreply.github.com>
2020-10-28 10:58:34 -0600

    Add files via upload

---
+++ restaurant.py
@@ -0,0 +1,8 @@
+class restaurante():
+ def __init__(self,NombreRestaurant, tipoCocina):
+    self.Nombre = NombreRestaurant
+    self.cocina= tipoCocina
+    def Restaurante(self):
+           print(self.Nombre() +" Restaurante")
+    def tipo(self):
+            print(self.cocina() +" El tipo de cocina es:")
