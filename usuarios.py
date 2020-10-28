6d2aa3bf00af2f22600e9ee5aebed64032256379
REM-IX <60319926+REM-IX@users.noreply.github.com>
2020-10-28 15:35:11 -0600

    Add files via upload

---
+++ usuarios.py
@@ -0,0 +1,8 @@
+class usuario():
+ def __init__(self,Nombre, ApellidoP, ApellidoM CURP, Fecha_nacimiento):
+    self.Nombre = Nombre
+    self.ApellidoP=A1
+    self.ApellidoM=A2
+    self.CURP=curp
+    def (self):
+        print(self.Nombre() +self.A1() +self.A2()+ self.curp())
